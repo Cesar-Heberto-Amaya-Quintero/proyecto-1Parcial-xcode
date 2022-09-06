4f404aa9d58a4c5cb488e2726fca3a9623d70f39
Fer <reikafer@gmail.com>
2022-09-06 07:17:17 -0700

    Initial Commit

---
+++ proyecto_1parcial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  proyecto_1parcial
+//
+//  Created by Alumno on 9/6/22.
+//  Copyright © 2022 ULSA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
